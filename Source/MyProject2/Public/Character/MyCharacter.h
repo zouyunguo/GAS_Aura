b293e3779d666d1f11ad7d6c3ac2f4ff6df56884
yunguo.zou <darkblueundertaker>
2026-05-11 11:44:59 +0800

    setting up repository

---
+++ Source/MyProject2/Public/Character/MyCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/CharacterBase.h"
+#include "MyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT2_API AMyCharacter : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MyProject2/Public/Player/AuraPlayerController.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+class UInputMappingContext;
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT2_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerController();
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+private:
+	TObjectPtr<UInputMappingContext> AuraContext;
+	
+};
