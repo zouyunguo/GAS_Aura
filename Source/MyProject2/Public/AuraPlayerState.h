9f88a6502911d0f69cc20cd2b9a7ce2efb4dc34c
yunguo.zou <darkblueundertaker>
2026-05-19 10:51:08 +0800

    Feature: Adding PlayerState class and arributeset and ability system component.

---
+++ Source/MyProject2/Public/AuraPlayerState.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT2_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	AAuraPlayerState();
+	
+};
