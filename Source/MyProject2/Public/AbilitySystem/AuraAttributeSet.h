b81910641fe8ccdaeeb612c9b04bcd08d30e5758
yunguo.zou <darkblueundertaker>
2026-05-20 17:59:58 +0800

    Feature: Adding attribute replication settings

--- Source/MyProject2/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/MyProject2/Public/AbilitySystem/AuraAttributeSet.h
@@ -13,5 +13,30 @@ UCLASS()
 class MYPROJECT2_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
+public:
+	UAuraAttributeSet();
+	// Setting replication mode
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	//setting function to bind when health is replicated
+	UPROPERTY(ReplicatedUsing = OnRep_Health,BlueprintReadOnly);
+	FGameplayAttributeData Health;
+	UPROPERTY(ReplicatedUsing = OnRep_MaxHealth, BlueprintReadOnly);
+	FGameplayAttributeData MaxHealth;
+	UPROPERTY(ReplicatedUsing = OnRep_Mana, BlueprintReadOnly);
+	FGameplayAttributeData Mana;
+	UPROPERTY(ReplicatedUsing = OnRep_MaxMana, BlueprintReadOnly);
+	FGameplayAttributeData MaxMana;
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 };
