ca9be667ddbecd62e98413191768f53e2072fb31
Yunguo <yunguo.zou@uzh.ch>
2026-07-24 19:42:43 +0800

    Feature: Adding health/mana bar widget

---
+++ Source/MyProject2/Public/UI/Widget/AuraUserWidget.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AuraUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT2_API UAuraUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	void SetWidgetController(UObject* widgetController);
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UObject> WidgetController;
+protected:
+	UFUNCTION(BlueprintImplementableEvent)
+	void WidgetControllerSet();
+};
